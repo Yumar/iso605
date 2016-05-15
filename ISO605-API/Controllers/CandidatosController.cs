7075f909f958277a71c163ba447f6ca3397f3e17
Yumarx <jumarpolanco@gmail.com>
2016-05-15 11:18:08 -0400

    added backend project

---
+++ ISO605-API/Controllers/CandidatosController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ISO605_API.Models;
+
+namespace ISO605_API.Controllers
+{
+    public class CandidatosController : ApiController
+    {
+        private ISO605Entities db = new ISO605Entities();
+
+        // GET: api/Candidatos
+        public IQueryable<candidato> Getcandidatoes()
+        {
+            return db.candidatoes;
+        }
+
+        // GET: api/Candidatos/5
+        [ResponseType(typeof(candidato))]
+        public IHttpActionResult Getcandidato(Guid id)
+        {
+            candidato candidato = db.candidatoes.Find(id);
+            if (candidato == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(candidato);
+        }
+
+        // PUT: api/Candidatos/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult Putcandidato(Guid id, candidato candidato)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != candidato.candidato_id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(candidato).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!candidatoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Candidatos
+        [ResponseType(typeof(candidato))]
+        public IHttpActionResult Postcandidato(candidato candidato)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.candidatoes.Add(candidato);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (candidatoExists(candidato.candidato_id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = candidato.candidato_id }, candidato);
+        }
+
+        // DELETE: api/Candidatos/5
+        [ResponseType(typeof(candidato))]
+        public IHttpActionResult Deletecandidato(Guid id)
+        {
+            candidato candidato = db.candidatoes.Find(id);
+            if (candidato == null)
+            {
+                return NotFound();
+            }
+
+            db.candidatoes.Remove(candidato);
+            db.SaveChanges();
+
+            return Ok(candidato);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool candidatoExists(Guid id)
+        {
+            return db.candidatoes.Count(e => e.candidato_id == id) > 0;
+        }
+    }
+}
