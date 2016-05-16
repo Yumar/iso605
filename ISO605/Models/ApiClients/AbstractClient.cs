2a3fcbd6e3aef7ecf364bedb5b30693643672937
Yumarx <jumarpolanco@gmail.com>
2016-05-15 20:07:53 -0400

    abstract clients

--- ISO605/Models/ApiClients/AbstractClient.cs
+++ ISO605/Models/ApiClients/AbstractClient.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,12 +10,27 @@ namespace ISO605.Models.ApiClients
 {
     public abstract class AbstractClient<T>
     {
-        private string BASE_URL;
-        public AbstractClient(string url) {
-            this.BASE_URL = url;
+        private HttpClient client;
+        private string API_URL;
+        public AbstractClient(string API_URL) {
+            this.API_URL = API_URL;
+
+            this.client = new HttpClient();
+            this.client.BaseAddress = new Uri(ClientUtil.BASE_URL);
+            this.client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
         }
         public IEnumerable<T> getAll() {
-            HttpClient client = new HttpClient();
+            IEnumerable<T> items = null;
+            try
+            {
+                HttpResponseMessage response = this.client.GetAsync(this.API_URL).Result;
+                if (response.IsSuccessStatusCode)
+                    items = (IEnumerable<T>)response.Content.ReadAsStreamAsync().Result;
+
+            }
+            catch {
+            }
+            return items;
         }
         public abstract T get(Guid id);
         public abstract T save(T item);
---
+++ ISO605/Models/ApiClients/ClientUtil.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISO605.Models.ApiClients
+{
+    class ClientUtil
+    {
+        public static string BASE_URL = "";
+    }
+}
